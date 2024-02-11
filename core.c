8bf2f08bd63322b5605766b1db63f50ac5b78e25
Sahak Grigoryan <sahak.ivasauskas@mif.vu.lt>
2024-02-12 01:26:55 +0200

    update: core.[ch] (initial pipeline)

--- core.c
+++ core.c
@@ -1,7 +1,49 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "core.h"
+#include "ins.h"
+#include "help.h"
+
+static struct ins *pipe_idec(uint32_t *op)
+{
+     struct ins *insn = NULL;
+     struct btn *node = NULL;
+
+     if (*op & BASEOP_MASK) {
+	  node  = find_btn(&base, *op);
+	  if (!node) {
+	       printf("node find fail\n");
+	       return NULL;
+	  }
+	  insn  = CONT_OF(node, struct ins, i_btn);
+	  *op >>= 2;
+     } else
+	  printf("Not implemented\n");
+     return insn;
+}
+
+static inline void pipe_pdec(struct ins *i, struct core *c, const uint32_t op)
+{
+     i->i_pdec(c, op);
+}
+
+void core_work(struct core *c)
+{
+     struct ins *insn = NULL;
+     uint32_t op = 0;
+
+     while (1) {
+	  /* TODO: fetch (need a fetchable mem region) */
+	  insn = pipe_idec(&op);
+	  if (!insn) {
+	       printf("idec fail\n");
+	       continue;
+	  }
+	  pipe_pdec(insn, c, op);
+     }
+}
 
 struct core *init_core(void)
 {
--- core.h
+++ core.h
@@ -5,17 +5,22 @@
 
 #include "csr.h"
 
+/* Number of General-Purpose Registers */
+#define GREGNUM (1 << GREGBIT)
+#define GREGBIT (5)
+
 /* Machine XLEN (TODO: 64,128-bit support) */
-#define MXLEN  (32)
-#define MXL32  (1)
+#define MXLEN   (32)
 
 /* Privileges (TODO: U, S, H) */
-#define M_PRV (3)
+#define M_PRV   (3)
 
 struct core {
-     struct csr co_misa;
+     struct csr  co_misa;
 
-     uint8_t    co_priv:2;
+     uint32_t    co_regs[GREGNUM];
+     uint32_t    co_pc;
+     uint8_t     co_priv:2;
 };
 
 struct core *init_core(void);
