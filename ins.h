1fa61e91576eee55206c81b91a5a4f636b2721a9
Sahak Grigoryan <sahak.ivasauskas@mif.vu.lt>
2024-03-16 00:30:49 +0200

    update: ins.h (const & macro)

--- ins.h
+++ ins.h
@@ -5,84 +5,75 @@
 #include "btree.h"
 #include "core.h"
 
-/* Macros for masking instruction bit-field regions */
+/* Bit-field constants */
 /* ******************************** */
-/* Mask values are determined considering base-op shifting (7) */
 
-/* This mask is applied after un-shifting base bits (2) */
+/* General const */
+#define REGSIZE     (5)
+#define OPSIZE      (7)
 #define BASEOP_MASK (0x1F)
+#define ISBASE_MASK (0x3)  /* TODO: move to C extension header */
+#define REG_MASK    (0x1F)
 
-/* This mask is applied on un-shifted ins */
-#define ISBASE_MASK (0x3)
+/* I-Type const */
+#define ITIM_SIZE   (12)
+#define ITF3_SIZE   (3)
+#define ITF3_MAX    (8)
+#define ITIM_MASK   (0xFFF)
+#define ITIS_MASK   (0x800)
+#define ITF3_MASK   (0x7)
+#define ITNG_TO32   (0xFFFFF000)
 
-/* Constant-region masks */
-#define RS2REG_MASK (0x3E000)
-#define RS1REG_MASK (0x1F00)
-#define RDREG_MASK  (0x1F)
+/* U-Type const */
+#define UTIM_SIZE   (20)
+#define UTIM_MASK   (0xFFFFF)
+#define UTIS_MASK   (0x80000)
+#define UT3021_MASK (0x7FE00)
+#define UT20_MASK   (0x80)
+#define UT1912_MASK (0xFF)
 
-/* I-Type Masks */
-#define IT_MASK_IMM (0x1FFE000)
-#define IT_MASK_F3  (0xE0)
-
-/* I-Type mask for 12-bit IMM convertion to 32-bit */
-#define IT_IMM_TO32 (0xFFFFF000)
-
-/* U-Type Masks */
-#define UT_MASK_IMM (0x1FFFFE0)
-
-/* ******************************** */
-
-/* Shift constants to acquire instruction bit-field region value */
-/* ******************************** */
-/* Shift values are determined considering base-op shifting (7) */
-
-/* Constant-region shift */
-#define RS2REG_SH   (13)
-#define RS1REG_SH   (8)
-#define RDREG_SH    (0)
-
-/* I-Type Shifts */
-#define IT_SH_IMM   (13)
-#define IT_SH_F3    (5)
-
-/* U-Type Shifts */
-#define UT_SH_IMM   (5)
+/* J-Type const */
+#define JTIS_MASK   (0x100000)
+#define JT11_MASK   (0x800)
+#define JT1912_OFFS (12)
+#define JT1001_OFFS (8)
+#define JTNG_TO32   (0xFFF00000)
 
 /* ******************************** */
-/* Other constants */
+/* Bit-field macros */
 
-/* OP bit field size */
-#define OP_BITS     (7)
-
-/* I-Type constants */
-#define IT_F3_MAX   (8)
-#define IT_IMM_BITS (12)
-
-
-#define GET_REG(c, op, R) ({					\
-	       uint8_t r = ((op & R##REG_MASK) >> R##REG_SH);	\
-	       &c->co_regs[r];					\
+/* General macro */
+#define PULL_REG(c, op) ({				\
+	       uint8_t r    = ((op) & REG_MASK);	\
+	       op         >>= REGSIZE;			\
+	       &(c)->co_regs[r];			\
 	  })
-#define GET_RS2(c, op)				\
-     GET_REG(c, op, RS2)
-#define GET_RS1(c, op)				\
-     GET_REG(c, op, RS1)
-#define GET_RD(c, op)				\
-     GET_REG(c, op, RD)
+#define PULL_RS2(c, op)				\
+     PULL_REG(c, op)
+#define PULL_RS1(c, op)				\
+     PULL_REG(c, op)
+#define PULL_RD(c, op)				\
+     PULL_REG(c, op)
 
-#define ITYPE_X(op, R)				\
-     ((op & IT_MASK_##R) >> IT_SH_##R)
-#define ITYPE_IMM(op)				\
-     ITYPE_X(op, IMM)
-#define ITYPE_F3(op)				\
-     ITYPE_X(op, F3)
+/* I-Type macro */
+#define PULL_ITIM(op) ({				\
+	       uint16_t im   = ((op) & ITIM_MASK);	\
+	       (op)        >>= ITIM_SIZE;		\
+	       im;					\
+	  })
+#define PULL_ITF3(op) ({				\
+	       uint8_t f3    = ((op) & ITF3_MASK);	\
+	       (op)        >>= ITF3_SIZE;		\
+	       f3;					\
+	  })
 
-#define UTYPE_X(op, R)				\
-     ((op & UT_MASK_##R) >> UT_SH_##R)
-#define UTYPE_IMM_NOSH(op)			\
-     (op & UT_MASK_IMM)
-#define UTYPE_IMM(op)				\
-     UTYPE_X(op, IMM)
+/* U-Type macro */
+#define PULL_UTIM(op) ({				\
+	       uint32_t im   = ((op) & UTIM_MASK);	\
+	       (op)        >>= UTIM_SIZE;		\
+	       im;					\
+	  })
+/* ******************************** */
 
 extern struct btr base;
 
